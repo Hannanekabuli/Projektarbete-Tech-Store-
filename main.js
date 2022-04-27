cd08c143368ff508bcdf1aecfdca3a644cc1350b
Hannane <hannanekabuli@gmail.com>
2022-04-27 20:40:14 +0100

    master

--- main.js
+++ main.js
@@ -1,159 +1,106 @@
-var listOfProducts;
-
-/** Get products from the json file and store it in a gobal variable */
-function loadProducts() {
-    fetch("./products.json")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(products) {
-        listOfProducts = products;
-        addProductsToWebpage();
-    });
-}
-
-
-function initSite() {
-    loadProducts();
-    // This would also be a good place to initialize other parts of the UI
-}
-
-/** Uses the loaded products data to create a visible product list on the website */
-function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
-
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when youve read them.
-}
-
-        
-var listOfProducts; [
-    {
-        title: "iPhone X",
-        description: "Last years phone from Apple with a beautiful all display front.",
-        image: "iPhoneX.png",
-        price: 11495
-    },{
-        title: "One Plus 5",
-        description: "Sleek and powerful smartphone from One Plus.",
-        image: "OnePlus5.png",
-        price: 4995
-    },{
-        title: "Galaxy S8",
-        description: "Really cool edge to edge smartphone from Samsung.",
-        image: "SamsungS8.png",
-        price: 7990
-    },{
-        title: "LG V30",
-        description: "Super nice and beautiful smartphone from LG.",
-        image: "LGV30.png",
-        price: 7495
-    }
-];
-
-
-function loadProducts  ()  {
-
-
- const productDiv = document.getElementById("./products.json")
-   .then(function(response) {
-        return response.json();
-    })
-    .then(function(products) {
-        listOfProducts = products;
-        addProductsToWebpage();
-    });
-}
-
-  productDiv.innerHTML = ""
-
- products.forEach((item, index)=> {
-    productDiv.innerHTML += 
-    <div class="product">
-  <div class="product_image">
-  <img src=${item.image}/>  
-  </div>
-  <h2 class="product_title">${item.name}</h2>
-  <p>Last years phone from Apple with a beautiful all display front.</p>
-    <h3 class="product_price">${item.price} kr</h3>
-     <button class="lägg till i kundvagnen">lägg till i kundvagnen<i class="fas fa-shopping-cart"></i></button>
-     <button class="cart_item-remove">ta bort<i class="fas fa-trash"></i></button>
-  </div> 
-    
- })
-
-
-let cart = {
-    item: [],
-    total: 0,
-}
-
-const renderProducts = () => {
-    const cartDiv = document.querySelector(".cart_item")
-    cartDiv.innerHTML= ""
-
-    const totalPriceEl = document.querySelector("cart_total-price")
-
-    let totalPrice = 0
-
-    if (cart.item.length===0) {
-        cartDiv.innerHTML = "kundvagnen är tomt"
-    }
-
-cart.item.forEach((item) => {
-    totalPrice += item.total
-
-    cartDiv.innerHTML +=
-    `
-    <div class="cart_total-price">${item,price}</div>
-                            
-    `
+fetch("./products.json")
+.then(response => {
+   return response.json();
 })
+.then(data => RenderData(data));
 
-totalPriceEl.innerHTML = `total: ${totalPriceEl} kr`
+var content =  document.querySelector('.content');
+var ShoppingCart = [];
+
+function RenderData(data){
+    var cartInfo = document.querySelector('.fa-shopping-cart');
+    var listOfData = JSON.parse(localStorage.getItem("product"));
+    if(listOfData)
+    cartInfo.innerText = " " + JSON.parse(localStorage.getItem("product")).length;
+
+    data.forEach((item, index) => {
+        var listItems = document.createElement("div");
+        var product = document.createElement("div");
+        var itemTitle =  document.createElement('h2');
+        var image =  document.createElement('img');
+        var description =  document.createElement('p');
+        var price =  document.createElement('div');
+        var quantity =  document.createElement('div');
+        var button =  document.createElement('button');
+
+        listItems.className = "item-list";
+        product.className = "product";
+        product.id = item.id;
+        itemTitle.className = "item-title";
+        image.className = "image-div";
+        description.className = "description";
+        price.className ="price";
+        quantity.className = "quantity";
+        button.className ="add-button";
+        button.type ="button";
+        button.textContent = "Add To Cart";
+        button.addEventListener("click", ClickHandler);
+
+        itemTitle.innerHTML = item.title;
+        image.src = "./assets/" + item.image;
+        description.innerHTML = item.description;
+        price.innerHTML = item.price;
+
+        product.append(itemTitle);
+        product.append(image);
+        product.append(description);
+        product.append(price);
+        product.append(quantity);
+        product.appendChild(button);
+        listItems.append(product);
+
+        listItems.append(product);
+        content.append(listItems);
+
+    });
+
 }
 
-const addToCart = (productIndex) => {
-    const product = products[productIndex]
+function ClickHandler(e){
+    var clickedItemValue =  e.target.parentNode.children;
+    var div = e.target.parentNode;
+    var data = {
+        id: div.id,
+        title: clickedItemValue[0].innerText,
+        image: clickedItemValue[1].src.split("/").slice(-1).pop(),
+        description: clickedItemValue[2].innerText,
+        price: clickedItemValue[3].innerText,
+        quantity: 1,
+    };
 
-    let existingProduct = false
-
-    let newCartItems = cart.item.reduce((state, item) => {
-      if (item.name === product.name) {
-          existingProduct = true
-
-          const newItem = {
-          ...item,
-          qty: item.qty + 1,
-          total: (item.qty + 1) * item.price
-      }  
-
-      return [...state, newItem]
-      }
-
-      return [...state,item]
-    }, [])
-
-    if (!existingProduct) {
-        newCartItems.push({
-            ...product,
-            qty:1,
-            total: product.price,
-        })
+    // load old data
+    var old = localStorage.getItem("product");
+    if(old != "[]" && old != "undefined")
+    {
+        var json = JSON.parse(old);
+        if(json)
+        json.forEach(d => ShoppingCart.push(d));
     }
 
-    cart = {
-        
-    }
+    ShoppingCart.forEach(el =>{
+        var result = el.id == data.id;
+        if(result)
+        {
+            data.quantity = data.quantity + el.quantity;
+            data.price = data.quantity  * data.price;
+            el.id = data.id;
+            el.title =data.title;
+            el.price = data.price;
+            el.description = data.description;
+            el.image= data.image;
+            el.quantity = data.quantity;
+        }
+    });
+
+    var result =  ShoppingCart.find((e) => e.id == data.id);
+    if(!result)
+        ShoppingCart.push(data);
+
+   if(ShoppingCart.length ==0)
+    ShoppingCart.push(data);
+
+    var cartInfo = document.querySelector('.fa-shopping-cart');
+    cartInfo.innerText = " " + ShoppingCart.length;
+    localStorage.setItem("product", JSON.stringify(ShoppingCart));
+    ShoppingCart = [];
 }
-
-renderProducts()
-renderCartItems()
-
-
-
-
